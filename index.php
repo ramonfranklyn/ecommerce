63a197bdc7be22756d6946c132d922e70222d1a5
ramonfranklyn <41338646+ramonfranklyn@users.noreply.github.com>
2021-05-03 16:16:07 -0300

    Identação do index
    Identação do index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php 
 
-require_once("vendor/autoload.php");
+	require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
+	use \Slim\Slim;
+	use \Hcode\Page;
 
-$app = new Slim();
+	$app = new Slim();
 
-$app->config('debug', true);
+	$app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$page = new Page();
+	$app->get('/', function() {
+	    
+		$page = new Page();
 
-	$page->setTpl("index");
+		$page->setTpl("index");
 
-});
+	});
 
-$app->run();
+	$app->run();
 
 ?>
