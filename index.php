f76ec5815d520cd95aa0604da82ee6c9d81775b2
ramonfranklyn <41338646+ramonfranklyn@users.noreply.github.com>
2021-05-06 19:47:14 -0300

    Identação do index

--- index.php
+++ index.php
@@ -1,64 +1,62 @@
 <?php 
 
-	session_start();
-	require_once("vendor/autoload.php");
+session_start();
+require_once("vendor/autoload.php");
 
-	use \Slim\Slim;
-	use \Hcode\Page;
-	use \Hcode\PageAdmin;
-	use \Hcode\Model\User;
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
-	$app = new \Slim\Slim();
+$app = new \Slim\Slim();
 
-	$app->config('debug', true);
+$app->config('debug', true);
 
-	$app->get('/', function() {
-	    
-		$page = new Hcode\Page();
+$app->get('/', function() {
+    
+	$page = new Hcode\Page();
 
-		$page->setTpl("index");
+	$page->setTpl("index");
 
-	});
+});
 
-	$app->get('/admin', function() {
-	    
-		User::verifyLogin();
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
 
-		$page = new Hcode\PageAdmin();
+	$page = new Hcode\PageAdmin();
 
-		$page->setTpl("index");
+	$page->setTpl("index");
 
-	});
+});
 
-	$app->get('/admin/login', function() {
-	    
-		$page = new Hcode\PageAdmin([
-			"header"=>false,
-			"footer"=>false
-		]);
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
 
-		$page->setTpl("login");
+	$page->setTpl("login");
 
-	});
+});
 
-	$app->post('/admin/login', function() {
+$app->post('/admin/login', function() {
 
-		User::login($_POST["login"], $_POST["password"]);
+	User::login($_POST["login"], $_POST["password"]);
 
-		header("Location: /admin");
-		exit;
+	header("Location: /admin");
+	exit;
 
-	});
+});
 
-	$app->get('/admin/logout', function() {
+$app->get('/admin/logout', function() {
 
-		User::logout();
+	User::logout();
 
-		header("Location: /admin/login");
-		exit;
+	header("Location: /admin/login");
+	exit;
 
-	});
+});
 
-	$app->run();
-
-	 
+$app->run();
